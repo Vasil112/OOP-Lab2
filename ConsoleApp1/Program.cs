9ea5eab8e2ff234aebcf9ecf746a81357c5a27c2
Vasil112 <105806255+Vasil112@users.noreply.github.com>
2025-09-15 18:39:12 +0300

    task1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Program
+{
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть значення x");
+            string x = Console.ReadLine();
+            Console.WriteLine("Введіть значення y");
+            string y = Console.ReadLine();
+            Console.WriteLine("Введіть значення z");
+            string z = Console.ReadLine();
+            double s;
+            if ((double.TryParse(x, out double Inx)) && (double.TryParse(y, out double Iny)) && (double.TryParse(z, out double Inz)))
+            {
+                s = Math.Pow(3 + Math.Tan(Inx) - (Iny / 2), (1 / 3)) / (Math.Pow(Inx, 2) + Math.Pow(Inz, 3) + 4);
+                Console.WriteLine(Math.Round(s,3));
+            }
+            else
+            {
+                Console.WriteLine("Невірні дані");
+            }
+            
+            }
+        }
+}
---
+++ OOPLab2/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System;
+
+namespace OOPLab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        { 
+        }
+
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if ((double.TryParse(Lab1.Text, out double x)) && (double.TryParse(Lab2.Text, out double y)) && (double.TryParse(Lab3.Text, out double z)))
+            {
+                double s = Math.Pow(3 + Math.Tan(x) - (y / 2), (1 / 3)) / (Math.Pow(x, 2) + Math.Pow(z, 3) + 4);
+                LabExt.Content = $"Результат: {Math.Round(s,3)}";
+            }
+            else 
+            {
+                LabExt.Content = "Невірні дані";
+            }
+            
+        }
+    }
+}
