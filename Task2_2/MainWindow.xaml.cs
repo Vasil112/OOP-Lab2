17b8e26b9791c77f775f27097c1cc701e5f3b4ba
Vasil112 <105806255+Vasil112@users.noreply.github.com>
2025-09-16 09:59:56 +0300

    add task2

---
+++ Task2_2/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task2_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void text1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void text2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void text3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            if ((double.TryParse(text1.Text, out double a)) && (double.TryParse(text2.Text, out double b)) && (double.TryParse(text3.Text, out double c)))
+            {
+                double d = b * b - (4 * a * c);
+                dis.Content = $"Дискримінант: {d}";
+                if (d > 0)
+                {
+                    double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                    double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                    root.Content = $"x1: {x1}, x2: {x2}";
+                }
+                else if (d == 0)
+                {
+                    double x = -b / (2 * a);
+                    root.Content = $"Корінь: {x}";
+                }
+                else
+                {
+                    root.Content = "Немає розв'язку";
+                }
+
+            }
+            else
+            {
+                root.Content = "Невірні дані";
+            }
+        }
+    }
+}
---
+++ task2_1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть значення a");
+            string a = Console.ReadLine();
+            Console.WriteLine("Введіть значення b");
+            string b = Console.ReadLine();
+            Console.WriteLine("Введіть значення c");
+            string c = Console.ReadLine();
+            if ((double.TryParse(a, out double Ina)) && (double.TryParse(b, out double Inb)) && (double.TryParse(c, out double Inc)))
+            {
+                double d = Inb * Inb - 4 * Ina * Inc;
+                Console.WriteLine($"Дискримінант: {d}");
+                if (d > 0)
+                {
+                    double x1 = (-Inb + Math.Sqrt(d)) / (2 * Ina);
+                    double x2 = (-Inb - Math.Sqrt(d)) / (2 * Ina);
+                    Console.WriteLine($"Корені рівнення: x1: {x1}, х2: {x2}");
+                }
+                else if (d == 0)
+                {
+                    double x = -Inb / (2 * Ina);
+                    Console.WriteLine($"Коренm рівнення: x: {x}");
+                }
+                else 
+                {
+                    Console.WriteLine("Не має розв'язку");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Невірні дані");
+            }
+        }
+    }
+}
